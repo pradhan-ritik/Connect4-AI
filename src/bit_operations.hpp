7437a12666d828a592966b13ed2f0c3c6bc243b0
rp <flipit001@gmail.com>
2026-09-02 21:20:55 -0700

    Replaced int with uint

--- src/bit_operations.hpp
+++ src/bit_operations.hpp
@@ -1,44 +1,44 @@
 #pragma once
 #include "config.hpp"
 
-inline int count_bits(BB bitboard) {
+inline uint count_bits(BB bitboard) {
     return __builtin_popcountll(bitboard);
 }
 
-inline int lsb(BB bitboard) {
+inline uint lsb(BB bitboard) {
     return __builtin_ctzll(bitboard);
 }
 
-inline int pop_lsb(BB& bitboard) {
-    int first = lsb(bitboard);
+inline uint pop_lsb(BB& bitboard) {
+    uint first = lsb(bitboard);
     bitboard &= bitboard - 1;
     return first;
 }
 
-inline BB bb(int pos) {
+inline BB bb(uint pos) {
     return 1ULL << pos;
 }
 
-inline bool get_bit(BB bitboard, int index) {
+inline bool is_bit_active(BB bitboard, uint index) {
     return bitboard & bb(index);
 }
 
-inline void set_bit_on(BB& bitboard, int index) {
+inline void set_bit_on(BB& bitboard, uint index) {
     bitboard |= bb(index);
 }
 
-inline void set_bit_off(BB& bitboard, int index) {
+inline void set_bit_off(BB& bitboard, uint index) {
     bitboard &= ~bb(index);
 }
 
-inline void toggle_bit(BB& bitboard, int index) {
+inline void toggle_bit(BB& bitboard, uint index) {
     bitboard ^= bb(index);
 }
 
 inline void print_BB(BB bitboard) {
     bool cur;
     for (int i = nSQUARES-1; i > -1; i--) {
-        cur = get_bit(bitboard, i);
+        cur = is_bit_active(bitboard, i);
         std::cout << cur << " ";
         if (i % nCOLUMNS == 0) {
             std::cout << "\n";
