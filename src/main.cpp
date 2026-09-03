c64e0f0b04e9916569108bb28dc5119903c685e0
rp <flipit001@gmail.com>
2026-09-02 21:21:13 -0700

    Test print board

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "config.hpp"
 #include "bit_operations.hpp"
 #include "tests.hpp"
+#include "board.hpp"
 
 
 int main() {
-    test_BBs();
+    Board b;
+    b.print_board();
     return 0;
 }
