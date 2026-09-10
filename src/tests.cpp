c40e92fa83c760ded14c4a8b279e6425cc684e66
rp <flipit001@gmail.com>
2026-09-09 17:53:11 -0700

    Test history

--- src/tests.cpp
+++ src/tests.cpp
@@ -34,11 +34,22 @@ void test_BBs() {
 void test_board() {
     Board b;
     b.print_board();
-    std::cout << "\n\n\n";
+    std::cout << "\n";
     b.make_move(1);
     b.make_move(1);
     b.make_move(1);
-    std::cout << "\n\n\n";
+    std::cout << "\n";
     b.print_board();
-    print_red_square();
+    History h = History();
+    for (int i = 0; i < 42; i++) {
+        h.append(5);
+        if (!(i % 10) || i == 18) { 
+            std::cout << "\n" << i << "\n";
+            print_BB64(h.get_hist((h.get_move_count()-1) > 20));
+        }
+    }
+
+    std::cout << "printing bitboards\n";
+    print_BB64(h.get_hist(0));
+    print_BB64(h.get_hist(1));
 }
--- src/tests.hpp
+++ src/tests.hpp
@@ -2,6 +2,7 @@
 #include "config.hpp"
 #include "bit_operations.hpp"
 #include "board.hpp"
+#include "history.hpp"
 
 void test_BBs();
 void test_board();
