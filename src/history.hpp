0ce954c49b8bbccbcf767a4f7c09b777fc1a9947
rp <flipit001@gmail.com>
2026-09-13 19:13:49 -0700

    Add get_last and pop_last to history

--- src/history.hpp
+++ src/history.hpp
@@ -5,12 +5,18 @@ struct History {
 private:
     BB hist[2];
     uint8_t move_count;
-    inline bool current_bitboard() { return move_count > 20; }
+
+    inline bool current_bitboard(bool last_move=false) { return (move_count-last_move) > 20; }
+    inline uint8_t offset(bool current_bitboard) { return 3*(move_count - 21*current_bitboard); }
 public:
-    inline BB get_hist(int n) { return hist[n]; }
+    // temp
+    inline BB get_hist(bool n) { return hist[n]; }
 
     History() : hist{0ULL, 0ULL}, move_count(0u) { }
     void append(BB col);
+    Move get_last();
+    Move pop_last();
+
     inline uint8_t get_move_count() {
         return move_count;
     }
