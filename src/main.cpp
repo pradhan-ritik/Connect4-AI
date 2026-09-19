ca78a145d8c5162a3801ddeb77408c29f23f6775
rp <flipit001@gmail.com>
2026-09-19 12:46:44 -0700

    Use interface

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include "bit_operations.hpp"
 #include "tests.hpp"
 #include "board.hpp"
+#include "interface.hpp"
 
 
 int main() {
-    test_board();
-    // Board b;
+    // test_board();
+    Board b;
+    Interface interface(&b);
+    interface.run();
     // b.print_board();
     return 0;
 }
