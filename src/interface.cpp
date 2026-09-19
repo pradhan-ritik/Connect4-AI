6071a442fbaf76ae01017b8801f61f0a435bd5a2
rp <flipit001@gmail.com>
2026-09-19 12:47:11 -0700

    Make interface

---
+++ src/interface.cpp
@@ -0,0 +1,47 @@
+#include "interface.hpp"
+
+void Interface::help() {
+    std::cout << "For playing: 1=A, 2=B, 3=C, 4=D, 5=E, 6=F, 7=G\n";
+    std::cout << "u to undo\n";
+    std::cout << "q to quit\n";
+    // std::cout << "p to print board\n";
+    std::cout << "h for this text\n";
+}
+
+void Interface::run() {
+    running = true;
+    help();
+    board->print_board();
+    while (running) {
+        std::cout << ": ";
+        char ch = getchar();
+        while ('\n'!=getchar());
+        std::cout << "\n";
+        run_command(ch);
+    }
+}
+
+void Interface::run_command(char ch) {
+    // Move
+    if (isdigit(ch)) {
+        // convert '1' - '7' to 0 - 6
+        Move m = ch - '1';
+        if (!move_in_range(m)) goto invalid;
+        board->make_move(m);
+        board->print_board();
+    }
+
+    else if (isalpha(ch)) {
+        if (ch == 'q') running = false;
+        else if (ch == 'h') help();
+        // else if (ch == 'p') board->print_board();
+        else if (ch == 'u') board->undo_move();
+        else { goto invalid; }
+    }
+
+
+    // if everything works, then end, if something goes wrong, "goto failiure"
+    return;
+invalid:
+    std::cout << "invalid command\n";
+}
