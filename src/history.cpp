32a65e4ca2546e9d704b18bb5fc77d973e377e8c
rp <flipit001@gmail.com>
2026-09-23 19:42:08 -0700

    Made the assertion more readable

--- src/history.cpp
+++ src/history.cpp
@@ -1,7 +1,7 @@
 #include "history.hpp"
 
 void History::append(BB col) {
-    assert(move_count <= 41);
+    assert(move_count < nSQUARES);
     bool cb = current_bitboard();
     // appends move, if second bitboard -21 from "list" position
     // std::cout << 3*(move_count - 21*cb) << " " << cb << " ";
